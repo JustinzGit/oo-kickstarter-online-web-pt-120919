9c6d19cbeaf4f8d1204aadd7ed09c304ee5c9412
Justin Branch <justinms@outlook.com>
2020-01-22 02:08:49 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,12 @@
 class Backer
-  attr_reader backed_projects
+  attr_reader :backed_projects
   
   def initialize
     @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
   end 
   
 end 
